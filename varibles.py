f9cf639a247cd6c002578f0c4441ebc0ee1d6913
Darwin709 <samsungcliente380@gmail.com>
2025-04-27 23:18:29 -0500

    practica

---
+++ varibles.py
@@ -0,0 +1,12 @@
+"""
+Operaciones
+"""
+
+# Operacoiones arimeticos
+print(F"suma: 20 + 10 = {20 + 10}")}
+print(F"Resta: 15 - 9 = {15 + 9}")
+print(F"Multiplicacion: 20 * 8 = {20 * 8}")
+print(F"Divicion: 16 / 4 = {16 / 4}")
+print(F"Modulo: 25 % 4 = {20 % 10}")
+print(F"Exponente: 30 ** 10 = {30 ** 10}")
+print(F"Divicion entera: 12 // 7 = {12 // 7}")
